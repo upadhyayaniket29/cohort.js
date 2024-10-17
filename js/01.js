6f53543e2b61c9d2b4310cd7b285b3431212dfa7
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2024-10-17 10:08:43 +0530

    Add files via upload

---
+++ js/01.js
@@ -0,0 +1,27 @@
+console.log('aniket')
+
+const accountId=14453
+let accountEmail="aniketuapdhyaya@google.com"
+var accountpassword="12345"
+accountCity="Jaipur"
+let accountstate;
+
+// accountId= 2 Not allowed since it is constant
+
+console.log(accountId)
+accountEmail="aniket@gif.com"
+console.log(accountEmail)
+accountpassword="212121"
+console.log(accountpassword)
+// console.table([aniket,upadhyay,])
+accountCity="bangaluru"
+console.log(accountCity)
+console.log(accountstate);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+
+
