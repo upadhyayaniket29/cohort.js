873ece1e580fa113824e73bdae484d5885178bc1
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2024-10-19 08:01:05 +0000

    function

--- js/cohort.js
+++ js/cohort.js
@@ -26,54 +26,69 @@
 
 // Write a progarm to greet a person with tefir first and last name
 
-let firstname='aniket';
-let lastname='upadhyay';
+// let firstname='aniket';
+// let lastname='upadhyay';
 
-console.log('Hello'+firstname + lastname)
+// console.log('Hello'+firstname + lastname)
 
 // Write a program that greet a person based on their gender
 
-let male,female;
-let gender;
-gender=male;
+// let male,female;
+// let gender;
+// gender=male;
 
-if(gender=male){
-    console.log('Hello'+firstname+lastname)
-}
-else
-console.log("she is female")
+// if(gender=male){
+//     console.log('Hello'+firstname+lastname)
+// }
+// else
+// console.log("she is female")
 
 // Write a program that counts from 0 -1000 and prints for loop
 
 
-for(let i=0;i<=1000;i++){
-    console.log(i);
+// for(let i=0;i<=1000;i++){
+//     console.log(i);
     
     
-}
+// }
 
 // Arrays in objects
 
-const person=[{
-    firstname:"ramesh",
-    gender:"male"
-},{
-    secondname:"aniket",
-    gender:"male"
-},{
-    thirdname:"upadhyaya",
-    gender:"male"
-}]
+// const person=[{
+//     firstname:"ramesh",
+//     gender:"male"
+// },{
+//     secondname:"aniket",
+//     gender:"male"
+// },{
+//     thirdname:"upadhyaya",
+//     gender:"male"
+// }]
 
-for(let i=0;i<person.length;i++)
-{
+// for(let i=0;i<person.length;i++)
+// {
     
-    if(person[i]["gender"]==male){
-        console.log(person[i]["firstname"]);
+//     if(person[i]["gender"]==male){
+//         console.log(person[i]["firstname"]);
         
-    }
+//     }
     
+// }
+
+//  Function 
+function sum(a,b){
+    const ans=a+b;
+    return ans;
 }
+
+const value=sum(1,2)
+console.log(value)
+
+// Callback functions 
+
+
+
+
 
 
 
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,1 +1,5 @@
-console.log(a)
+for(let i=0;i<=1000;i++){
+//     console.log(i);
+    
+    
+// }
