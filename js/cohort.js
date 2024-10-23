65af7597aeccba09f33e83143d2b55e51fa753fa
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2024-10-23 10:46:13 +0000

    array commits

--- js/cohort.js
+++ js/cohort.js
@@ -244,3 +244,74 @@ explainParseFloat("42px");
 randomparse=parseFloat("abc")
 console.log(randomparse)
 
+
+// Array 
+
+//Push Example
+function pushExample(arr, element) {
+  console.log("Original Array:", arr);
+
+  arr.push(element);
+  console.log("After push:", arr);
+}
+pushExample([1, 2, 3], 4);
+pushExample([1,2,3,4],5)
+
+// Pop Example 
+
+function popExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.pop();
+  console.log("After pop:", arr);
+}
+popExample([1, 2, 3]);
+
+function shiftExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.shift();
+  console.log("After shift:", arr);
+}
+shiftExample([1, 2, 3]);
+
+
+function shiftexample(arr,element){
+
+  console.log("orginal array",arr);
+  arr.unshift(element);
+  console.log("after shift",arr);
+}
+shiftexample([1,2,3,4,5,6],0)
+
+
+// Concat
+function concatExample(arr1, arr2) {
+  console.log("Original Arrays:", arr1, arr2);
+
+  let arr3 = arr1.concat(arr2);
+  console.log("After concat:", arr3);
+}
+concatExample([1, 2, 3], [4, 5, 6]);
+
+
+// forEach
+
+
+
+
+
+
+
+
+// forEach()
+function forEachExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.forEach(function(item, index) {
+    console.log(item, index);
+  });
+}
+forEachExample([1, 2, 3,4,5,6]);
+
+
