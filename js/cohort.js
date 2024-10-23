6820397533cbb64754368f2f6b063601e0883069
Aniket Upadhyay <upadhyayaniket29@gmail.com>
2024-10-23 10:17:22 +0000

    parseInt and parsefloat

--- js/cohort.js
+++ js/cohort.js
@@ -213,3 +213,34 @@ function getSlice(str, start, end) {
 //   !Important
 
 //   difference between slice and substring can be seen when start is greater than stop
+
+
+function explainParseInt(value) {
+  console.log("Original Value:", value);
+  let result = parseInt(value);
+  console.log("After parseInt:", result);
+}
+
+explainParseInt("42");
+explainParseInt("42px");
+explainParseInt("3.14");
+
+
+newparse=parseInt("123",48);
+console.log(newparse)
+
+
+function explainParseFloat(value) {
+  console.log("Original Value:", value);
+  let result = parseFloat(value);
+  console.log("After parseFloat:", result);
+}
+
+// Example Usage for parseFloat
+explainParseFloat("3.14");
+explainParseFloat("42");
+explainParseFloat("42px");
+
+randomparse=parseFloat("abc")
+console.log(randomparse)
+
